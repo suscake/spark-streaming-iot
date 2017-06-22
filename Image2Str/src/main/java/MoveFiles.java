3c36817f3a48f06291528a188926376766c8acd6
Cloud User <centos@cjc2.novalocal>
2017-06-22 10:45:17 +0800

    add new project Image2Str

---
+++ Image2Str/src/main/java/MoveFiles.java
@@ -0,0 +1,24 @@
+import java.io.*;
+
+public class MoveFiles {
+    public static void main(String[] args)throws IOException{
+        String fromPathPrefix="/home/jiacheng/testdata/type2_test1/";
+        String IMG_PATH_PHY_PREFIX="/mnt/DP_disk";
+        String IMG_PATH_PHY_SUFFIX="/jiacheng/testdata/";
+        InputStream in =null;
+        byte[] data=null;
+        for(int i=1;i<=10000;i++){
+            String imgName="type2_test1_"+i+".jpg";
+            in=new FileInputStream(fromPathPrefix+imgName);
+            data=new byte[in.available()];
+            in.read(data);
+            in.close();
+            int diskId=i%7+1;
+            String imgPath=IMG_PATH_PHY_PREFIX+diskId+IMG_PATH_PHY_SUFFIX+imgName;
+            OutputStream out =new FileOutputStream(imgPath);
+            out.write(data);
+            out.flush();
+            out.close();
+        }
+    }
+}
